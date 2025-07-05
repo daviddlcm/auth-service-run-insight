68d310010d45fd45d98ad2fc9f820da332c4df17
david <daviddelacruzmorales42@gmail.com>
2025-07-05 00:46:25 -0600

    uppercase roles and genders fixed

--- src/migrations/20250701042612-create-roles.js
+++ src/migrations/20250701042612-create-roles.js
@@ -9,7 +9,7 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.createTable("Roles", {
+    await queryInterface.createTable("roles", {
       id: {
         type: Sequelize.INTEGER.UNSIGNED,
         primaryKey: true,
